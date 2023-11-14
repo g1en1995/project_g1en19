06afa2c9e2a990545d96e31c761363b5176ee022
g1en1995 <glencf19@gmail.com>
2023-11-14 17:38:40 -0500

    secondCommit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+from vega_datasets import data
 
 """
 # Welcome to Streamlit!
@@ -38,5 +39,63 @@ In the meantime, below is an example of what you can do with just a few lines of
 #         color=alt.Color("idx", legend=None, scale=alt.Scale()),
 #         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
 #     ))
+
+
+airports = data.airports.url
+flights_airport = data.flights_airport.url
+
+states = alt.topo_feature(data.us_10m.url, feature="states")
+
+select_city = alt.selection_point(
+    on="mouseover", nearest=True, fields=["origin"], empty=False
+)
+
+lookup_data = alt.LookupData(
+    airports, key="iata", fields=["state", "latitude", "longitude"]
+)
+
+background = alt.Chart(states).mark_geoshape(
+    fill="lightgray",
+    stroke="white"
+).properties(
+    width=750,
+    height=500
+).project("albersUsa")
+
+connections = alt.Chart(flights_airport).mark_rule(opacity=0.35).encode(
+    latitude="latitude:Q",
+    longitude="longitude:Q",
+    latitude2="lat2:Q",
+    longitude2="lon2:Q"
+).transform_lookup(
+    lookup="origin",
+    from_=lookup_data
+).transform_lookup(
+    lookup="destination",
+    from_=lookup_data,
+    as_=["state", "lat2", "lon2"]
+).transform_filter(
+    select_city
+)
+
+points = alt.Chart(flights_airport).mark_circle().encode(
+    latitude="latitude:Q",
+    longitude="longitude:Q",
+    size=alt.Size("routes:Q").legend(None).scale(range=[0, 1000]),
+    order=alt.Order("routes:Q").sort("descending"),
+    tooltip=["origin:N", "routes:Q"]
+).transform_aggregate(
+    routes="count()",
+    groupby=["origin"]
+).transform_lookup(
+    lookup="origin",
+    from_=lookup_data
+).transform_filter(
+    (alt.datum.state != "PR") & (alt.datum.state != "VI")
+).add_params(
+    select_city
+)
+
+(background + connections + points).configure_view(stroke=None)
 
 
