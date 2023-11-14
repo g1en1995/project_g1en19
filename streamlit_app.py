da538799022d33d2456f0513b5c6bfa73f0fd362
g1en1995 <glencf19@gmail.com>
2023-11-14 17:59:30 -0500

    buggingAround

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-from vega_datasets import data
 
 """
 # Welcome to Streamlit!
@@ -14,88 +13,28 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-# num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-# num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
+num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
 
-# indices = np.linspace(0, 1, num_points)
-# theta = 2 * np.pi * num_turns * indices
-# radius = indices
+indices = np.linspace(0, 1, num_points)
+theta = 2 * np.pi * num_turns * indices
+radius = indices
 
-# x = radius * np.cos(theta)
-# y = radius * np.sin(theta)
+x = radius * np.cos(theta)
+y = radius * np.sin(theta)
 
-# df = pd.DataFrame({
-#     "x": x,
-#     "y": y,
-#     "idx": indices,
-#     "rand": np.random.randn(num_points),
-# })
+df = pd.DataFrame({
+    "x": x,
+    "y": y,
+    "idx": indices,
+    "rand": np.random.randn(num_points),
+})
 
-# st.altair_chart(alt.Chart(df, height=700, width=700)
-#     .mark_point(filled=True)
-#     .encode(
-#         x=alt.X("x", axis=None),
-#         y=alt.Y("y", axis=None),
-#         color=alt.Color("idx", legend=None, scale=alt.Scale()),
-#         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-#     ))
-
-
-airports = data.airports.url
-flights_airport = data.flights_airport.url
-
-states = alt.topo_feature(data.us_10m.url, feature="states")
-
-select_city = alt.selection_point(
-    on="mouseover", nearest=True, fields=["origin"], empty=False
-)
-
-lookup_data = alt.LookupData(
-    airports, key="iata", fields=["state", "latitude", "longitude"]
-)
-
-background = alt.Chart(states).mark_geoshape(
-    fill="lightgray",
-    stroke="white"
-).properties(
-    width=750,
-    height=500
-).project("albersUsa")
-
-connections = alt.Chart(flights_airport).mark_rule(opacity=0.35).encode(
-    latitude="latitude:Q",
-    longitude="longitude:Q",
-    latitude2="lat2:Q",
-    longitude2="lon2:Q"
-).transform_lookup(
-    lookup="origin",
-    from_=lookup_data
-).transform_lookup(
-    lookup="destination",
-    from_=lookup_data,
-    as_=["state", "lat2", "lon2"]
-).transform_filter(
-    select_city
-)
-
-points = alt.Chart(flights_airport).mark_circle().encode(
-    latitude="latitude:Q",
-    longitude="longitude:Q",
-    size=alt.Size("routes:Q").legend(None).scale(range=[0, 1000]),
-    order=alt.Order("routes:Q").sort("descending"),
-    tooltip=["origin:N", "routes:Q"]
-).transform_aggregate(
-    routes="count()",
-    groupby=["origin"]
-).transform_lookup(
-    lookup="origin",
-    from_=lookup_data
-).transform_filter(
-    (alt.datum.state != "PR") & (alt.datum.state != "VI")
-).add_params(
-    select_city
-)
-
-(background + connections + points).configure_view(stroke=None)
-
-
+st.altair_chart(alt.Chart(df, height=700, width=700)
+    .mark_point(filled=True)
+    .encode(
+        x=alt.X("x", axis=None),
+        y=alt.Y("y", axis=None),
+        color=alt.Color("idx", legend=None, scale=alt.Scale()),
+        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
+    ))
