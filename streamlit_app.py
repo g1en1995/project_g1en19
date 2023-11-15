e37c1d918b74b3f95c8b5f8f813edbe9bdf5d66f
g1en1995 <glencf19@gmail.com>
2023-11-14 22:21:55 -0500

    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ if st.button('Get Population Projections'):
     .encode(
         x = "AGE:O",
         y = "POP:Q",
-        color=alt.Color('orange')
+        color=alt.ColorName('violet')
     ).properties(width=600))
 
     # st.altair_chart((line).properties(
