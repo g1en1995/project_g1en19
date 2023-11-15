b5ea216e73b940d2ca3d2184fbab6e91ffbc9dfc
g1en1995 <glencf19@gmail.com>
2023-11-14 22:43:29 -0500

    v

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,17 +58,16 @@ st.write(f'Age window from 0 to {age}')
 if st.button('Get Population Projections'):
     response = requests.get(f'https://api.census.gov/data/timeseries/idb/1year?get=NAME,GENC,POP&YR=2023:{years}&AGE=0:{age}&SEX=0&for=genc+standard+countries+and+areas:{country}&key={apiKey}')
     # response.raise_for_status()
+    # ['NAME', 'GENC', 'POP', 'YR', 'AGE', 'SEX', 'genc standard countries and areas']
     jsonData = json.loads(response.text)
-    df = pd.DataFrame(jsonData)
+    df = pd.DataFrame(jsonData[1:])
     st.dataframe(df)
 
-    st.altair_chart(alt.Chart(df)
-    .mark_bar()
-    .encode(
-        x = "AGE:O",
-        y = "POP:Q",
-        color=alt.value('orange')
-    ).properties(width=600))
+    st.altair_chart(alt.Chart(df).mark_bar().encode(
+    x = alt.X("4:O").sort(),
+    y = "2:Q",
+).properties(width=1000)
+)
 
     # st.altair_chart((line).properties(
     # title= f"Population of {country} from 2003 to {years} in total",
