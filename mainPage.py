21ff83199dac8c532d1d327b5c479ed6af965423
g1en1995 <glencf19@gmail.com>
2023-11-21 18:52:28 -0500

    foodPages

--- mainPage.py
+++ mainPage.py
@@ -6,39 +6,14 @@ import requests
 import json
 
 """
-# Welcome :heart:.
+# Welcome :heart:
 
 This is an effort to document, explore my different areas of interests. Visualize to showcase my point of view.
 
-Thanks to Streamlit for building the platform. 
+-- Thanks to Streamlit for building the platform. --
 """
 
-# num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-# num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-# indices = np.linspace(0, 1, num_points)
-# theta = 2 * np.pi * num_turns * indices
-# radius = indices
-
-# x = radius * np.cos(theta)
-# y = radius * np.sin(theta)
-
-# df = pd.DataFrame({
-#     "x": x,
-#     "y": y,
-#     "idx": indices,
-#     "rand": np.random.randn(num_points),
-# })
-
-# st.altair_chart(alt.Chart(df, height=700, width=700)
-#     .mark_point(filled=True)
-#     .encode(
-#         x=alt.X("x", axis=None),
-#         y=alt.Y("y", axis=None),
-#         color=alt.Color("idx", legend=None, scale=alt.Scale()),
-#         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-#     ))
-
+# US census bureau API Key
 apiKey = '49151930da411856c561cc751ee2945a6a5f249a'
 
 country = 'IN'
---
+++ pages/FoodReceipes.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown("Food recipes to try")
+st.sidebar.markdown("Food reciepes.")
+
+st.write("Pomfret red curry - https://www.youtube.com/watch?v=kicpOtKiBnE ")
+
