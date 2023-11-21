9e1ee729ef9714c453dbb0492823b4b4d35298f9
g1en1995 <glencf19@gmail.com>
2023-11-20 20:21:33 -0500

    semanticPageUpdates

--- pages/TechnologyBrother.py
+++ pages/TechnologyBrother.py
@@ -2,3 +2,5 @@ import streamlit as st
 
 st.markdown(" Writing down key learnings from technologies. And developing portfolios and skill.")
 st.sidebar.markdown("Understanding Technology")
+
+#streamlit_app
