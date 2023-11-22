954d5d7cf659f4e0954f82c42bbc3de8acae7dbe
g1en1995 <glencf19@gmail.com>
2023-11-22 13:43:51 -0500

    food

--- pages/FoodReceipes.py
+++ pages/FoodReceipes.py
@@ -4,4 +4,6 @@ st.markdown("Food recipes to try")
 st.sidebar.markdown("Food reciepes.")
 
 st.write("Pomfret red curry - https://www.youtube.com/watch?v=kicpOtKiBnE ")
+st.write("Hyderabadi Chicken Green curry - https://youtu.be/ElMhRll8JCc?feature=shared")
+
 
